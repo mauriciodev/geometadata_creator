bb40579bef492906683698ab746f3b57c81da481
marcinmss <marciomsantoss@gmail.com>
2024-04-15 18:05:50 -0300

    O Swagger está rodando no url .../api/docs/

--- app/geometadata_creator/urls.py
+++ app/geometadata_creator/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
--- gmd_creator_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class GmdCreatorAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "gmd_creator_app"
