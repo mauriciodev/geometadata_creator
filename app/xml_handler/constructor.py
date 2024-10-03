8e4d2cab6cdedcc1e194467b823deac6c9f71a9b
marcinmss <marciomsantoss@gmail.com>
2024-10-03 19:47:46 -0300

    fix: changed the old_path to serch string function so that is uses a better, more robust search pattern

--- app/xml_handler/constructor.py
+++ app/xml_handler/constructor.py
@@ -1,15 +1,11 @@
 """ File containing the code for constructing the an iso xml file based on its fields """
 
-from lxml import etree
-from re import split
+from lxml import etree as et
+from re import findall
 
 
-def to_seach_string(old_path: str) -> str:
-    return "./{*}" + "/{*}".join(
-        x
-        for x in split(r"/\w+:", old_path.replace(" ", "_"))
-        if x not in {"", "MD_Metadata"}
-    )
+def old_path_to_search_string(old_path: str) -> str:
+    return "./" + "/".join("{*}" + t for _, t in findall(r"/(\w+):(\w+)", old_path)[1:])
 
 
 def construct_xml(template_path: str, old_paths: list[str], values: list[str]) -> bytes:
