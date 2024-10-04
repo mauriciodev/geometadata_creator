7bed77554bdb743e974a6502804cc2439579c614
marcinmss <marciomsantoss@gmail.com>
2024-10-04 14:53:09 -0300

    refactor: Mudança do diretório de armazenamento e ajute de volumes no docker_compose

--- app/geometadata_creator/settings.py
+++ app/geometadata_creator/settings.py
@@ -126,9 +126,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
+STATIC_URL = "/static/static/"
+MEDIA_URL = "/static/media/"
+
+MEDIA_ROOT = "/vol/app/media"
+STATIC_ROOT = "/vol/app/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
