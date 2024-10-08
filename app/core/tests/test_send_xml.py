f3a079d06275b28ecfe44b8176e552e912fdc1ad
marcinmss <marciomsantoss@gmail.com>
2024-10-08 15:50:39 -0300

    feat: Added a test to send_xml endpoint for checking that file not found has proper error handling

--- app/core/tests/test_send_xml.py
+++ app/core/tests/test_send_xml.py
@@ -1,10 +1,13 @@
+from rest_framework import status
 from rest_framework.test import APITestCase
 from core.models import GeospatialResource
 from django.contrib.auth import get_user_model
 from django.core.files.base import ContentFile
 from file_handler.utils import get_example_raster
+from django.test import tag
 
 
+@tag("new")
 class GeospatialResourceUploadEndpointTests(APITestCase):
     fixtures = [
         "index_map",
@@ -29,3 +32,10 @@ class GeospatialResourceUploadEndpointTests(APITestCase):
     def tearDown(self):
         self.obj.delete()
         return super().tearDown()
+
+    def test_file_not_found_returns_404(self):
+        """Assert that if the file is not found it will return a 404 error"""
+        url = f"/geoproduct/{self.obj.id + 1}/build_metadata/"
+        payload = {"metadata_fields": [], "product_type": self.product_type_id}
+        response = self.client.post(url, payload, format="json")
+        self.assertEqual(status.HTTP_404_NOT_FOUND, response.status_code)
